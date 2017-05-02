dc89c4719f65bfddbe52e564236990fb59e6a33d
Manu <Manu@MacBook-Pro-de-Manu.local>
2017-05-02 20:16:28 -0300

    LA HORA en la congreso

---
+++ hora.c
@@ -0,0 +1,28 @@
+/* localtime example */
+#include <stdio.h>      /* puts, printf */
+#include <time.h>       /* time_t, struct tm, time, localtime */
+#include <stdlib.h>
+
+int main ()
+{
+
+  pid_t pid = fork();
+
+  if(pid){ 
+  	//Padre
+  	wait(NULL);
+  	time_t rawtime;
+  	struct tm * timeinfo;
+  	time (&rawtime);
+  	timeinfo = localtime (&rawtime);
+  	printf ("Señal SIGUSR1 recibida: %s", asctime(timeinfo));
+  	
+
+  } else {
+  	printf("Programa ahora ejecutandose. PID=%d\n",getpid());
+  	printf("Listo para recibir la señal SIGUSR1.");
+
+  	for(;;);
+  }
+return 0;
+}
